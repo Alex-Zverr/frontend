d0e8079adce3affe98de92b3195925929f255853
Alex <Cah9H@live.ru>
2024-05-02 23:50:30 +0300

    redirect to Home page

--- src/Pages/Login.tsx
+++ src/Pages/Login.tsx
@@ -1,5 +1,5 @@
 import { SubmitHandler, useForm } from "react-hook-form"
-import { Link } from "react-router-dom"
+import { Link, Navigate, useLocation } from "react-router-dom"
 import FormInput from "../Components/FormInput"
 import { ILogin } from "../Interface/auth"
 import { useMutation } from "@tanstack/react-query"
@@ -7,28 +7,28 @@ import { loginUser } from "../Services/auth.services"
 
 
 const Login = () => {
+    const location = useLocation()
     const { 
         register, 
         handleSubmit,
-        reset,
         formState: { errors } 
     } = useForm<ILogin>({
         mode: 'onBlur',
     })
 
-    const { mutate, isSuccess, data } = useMutation({
-        mutationFn: async (data: ILogin) => await loginUser(data),
+    const { mutate, isSuccess, data, isPending } = useMutation({
+        mutationFn: async (dataUser: ILogin) => await loginUser(dataUser),
     })
 
-    const onSubmit: SubmitHandler<ILogin> = loginData => {
+    const onSubmit: SubmitHandler<ILogin> = async loginData => {
         mutate(loginData);
+    }
 
-        if(isSuccess) {
-            console.log(data);
-            localStorage.setItem('AccessToken', `${data.token_type} ${data.access_token}`);
-            reset();
-        }
-
+    if(isSuccess) {
+        localStorage.setItem('AccessToken', `${data.token_type} ${data.access_token}`);
+        console.log('redirect');
+        
+        return <Navigate replace to="/" state={{ from: location }} />
     }
 
     return (
@@ -36,41 +36,45 @@ const Login = () => {
             <div className="container">
                 <div className="login__holder">
                     <span className="login__title">Войти в аккаунт</span>
-                    <form 
-                        className="login__form" 
-                        onSubmit={handleSubmit(onSubmit)}
-                    >
-                        <FormInput 
-                            errors={errors} 
-                            register={register}
-                            name="username"
-                            placeholder="Логин"
-                            type="text"
-                            params={{
-                                required: "Поле обязательно для заполнения",
-                                minLength: {
-                                    value: 3,
-                                    message: "Минимальная длина логина 3 символов"
-                                }
-                            }}
-                        />
-                        <FormInput 
-                            errors={errors} 
-                            register={register}
-                            name="password"
-                            placeholder="Пароль"
-                            type="password"
-                            params={{
-                                required: "Поле обязательно для заполнения",
-                                minLength: {
-                                    value: 6,
-                                    message: "Минимальная длина логина 6 символов"
-                                }
-                            }}
-                        />
-                        <button className="login__btn btn">Войти</button>
-                        <Link className="login__link" to="/registration">Зарегистрироваться</Link>
-                    </form>
+                    {(isPending) ? (
+                        <div>Загрузка...</div>
+                    ) : (
+                        <form 
+                            className="login__form" 
+                            onSubmit={handleSubmit(onSubmit)}
+                        >
+                            <FormInput 
+                                errors={errors} 
+                                register={register}
+                                name="username"
+                                placeholder="Логин"
+                                type="text"
+                                params={{
+                                    required: "Поле обязательно для заполнения",
+                                    minLength: {
+                                        value: 3,
+                                        message: "Минимальная длина логина 3 символов"
+                                    }
+                                }}
+                            />
+                            <FormInput 
+                                errors={errors} 
+                                register={register}
+                                name="password"
+                                placeholder="Пароль"
+                                type="password"
+                                params={{
+                                    required: "Поле обязательно для заполнения",
+                                    minLength: {
+                                        value: 6,
+                                        message: "Минимальная длина логина 6 символов"
+                                    }
+                                }}
+                            />
+                            <button className="login__btn btn">Войти</button>
+                            <Link className="login__link" to="/registration">Зарегистрироваться</Link>
+                        </form>
+                    )}
                 </div>
             </div>
         </div>
